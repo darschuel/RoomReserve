6d7ca8867d04690545610e6a7b41af279f74bfbb
darschuel <diego.araujo.fifa@hotmail.com>
2026-01-15 14:21:44 +0100

    postgres setup mit flyway und entity template gelöscht

---
+++ backend/roomreserve/src/test/java/ch/diegoaraujo/roomreserve/health/HealthResourceIT.java
@@ -0,0 +1,8 @@
+package ch.diegoaraujo.roomreserve.health;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HealthResourceIT extends HealthResourceTest {
+    // Execute the same tests but in packaged mode.
+}
